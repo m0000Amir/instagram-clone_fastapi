8dad8fd067b973af9b6cf4c71f75af5230e783f6
m0000a <mukhtarov.amir.a@gmail.com>
2021-12-23 00:41:21 +0300

    finished caption and comments

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,45 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Post from './Post';
+
+
+const BASE_URL = 'http://localhost:8000/'
+
 
 function App() {
-
-  const BASE_URL = "http://localhost:3000/"
+  
 
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     fetch(BASE_URL + "post/all")
+    .then(response => {
+      const json = response.json();
+      console.log(json);
+      if (response.ok) {
+        return json
+      }
+      throw response
+    })
+    .then(data =>{
+      setPosts(data)
+    })
+    .catch(error => {
+      console.log(error);
+      alert(error)
+    })
   }, [])
 
   return (
-    "Hello, World!"
+    <div className="app_posts">
+      {
+        posts.map(post => (
+          <Post 
+            post = {post}
+          />
+        ))
+      }
+    </div>
   );
 }
 
---
+++ ui/src/Post.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react';
+import "./Post.css"
+
+
+const BASE_URL = 'http://localhost:8000/'
+
+
+function Post({ post }) {
+
+    const [imageUrl, setImageUrl] = useState('')
+    const [comments, setComments] = useState([])
+
+    useEffect(() => {
+        if (post.image_url_type == "absolute") {
+            setImageUrl(post.image_url)
+        } else {
+            setImageUrl(BASE_URL + post.image_url)
+        }
+    })
+
+    useEffect(() => {
+        setComments(post.comments)
+    }, [])
+
+
+    return (
+        <div className="post">
+            <img 
+                className="posts_image"
+                src={imageUrl}
+            />
+            <h4 className="post_text">{post.caption}</h4>
+
+            <div className="post_comments">
+                {
+                    comments.map((comment) => (
+                        <p>
+                            <strong>{comment.username}: </strong>{comment.text}
+                        </p>
+                    ))
+                }
+
+            </div>
+        </div>
+    )
+}
+
+export default Post
