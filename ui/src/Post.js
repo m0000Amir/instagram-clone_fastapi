a4d41087cdac19378355fc4bc9552c62e59627a8
m0000a <mukhtarov.amir.a@gmail.com>
2021-12-23 18:19:22 +0300

    finished signup functionality

--- ui/src/Post.js
+++ ui/src/Post.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import "./Post.css"
+import { Avatar, Button} from "@material-ui/core"
 
 
 const BASE_URL = 'http://localhost:8000/'
@@ -11,7 +12,7 @@ function Post({ post }) {
     const [comments, setComments] = useState([])
 
     useEffect(() => {
-        if (post.image_url_type == "absolute") {
+        if (post.image_url_type === "absolute") {
             setImageUrl(post.image_url)
         } else {
             setImageUrl(BASE_URL + post.image_url)
@@ -25,8 +26,16 @@ function Post({ post }) {
 
     return (
         <div className="post">
+            <div className="post_header">
+                <Avatar alt="Catalin" src=""/>
+                <div className="post_headerInfo">
+                    <h3>{ post.user.username }</h3> 
+                    <Button className='post_delete'>Delete</Button> 
+                </div>
+            </div>
+
             <img 
-                className="posts_image"
+                className="post_image"
                 src={imageUrl}
             />
             <h4 className="post_text">{post.caption}</h4>
